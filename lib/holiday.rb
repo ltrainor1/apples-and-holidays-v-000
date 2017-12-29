3ab59f4b67fce6e77776aae6e145858f87586943
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-29 19:02:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,9 @@ holiday_hash.each do |season, holiday|
     cap = holiday_s
     if holiday_s.include? "_"
       holiday_s = holiday_s.split("_")
-      cap = holiday_s.each {|word| word.capitalize}
+      cap = cap.each do |word| 
+        word.capitalize
+      end 
       cap = cap.join(" ")
     end 
     puts "  #{cap}: #{supplies}"
